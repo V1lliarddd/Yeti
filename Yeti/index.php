df2c14671350260db291cd3806b115f8f5344f9c
V1lliarddd <114908760+V1lliarddd@users.noreply.github.com>
2022-10-04 09:18:15 +0300

    Yeti-done

--- Yeti/index.php
+++ Yeti/index.php
@@ -27,6 +27,22 @@ $user_name = 'Даниил';
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
+        <?php if($is_auth==1):?>
+            <div class="user-menu__logged">
+                <p><?php echo $user_name?></p>
+                <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                <a class="user-menu__logout" href="#">Выход</a>
+            </div>
+        <?php else:?>
+            <ul class="user-menu__list">
+                <li class="user-menu__item">
+                    <a href="#">Регистрация</a>
+                </li>
+                <li class="user-menu__item">
+                    <a href="#">Выход</a>
+                </li>
+            </ul>
+        <?php endif ?>
         </nav>
     </div>
 </header>
