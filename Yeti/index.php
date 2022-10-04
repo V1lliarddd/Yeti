6806214b28721d0bbf930093d217322271750617
V1lliarddd <114908760+V1lliarddd@users.noreply.github.com>
2022-10-04 14:05:27 +0300

    Yeti arrays & loops done

--- Yeti/index.php
+++ Yeti/index.php
@@ -2,6 +2,23 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Даниил';
+
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+$goods = [
+    [
+        "name" => "2014 Rossignol District Snowboard",
+        "categories" =>"Доски и лыжи",
+        "price" => 10999 ,
+        "img_url" => "img/lot-1.jpg"
+    ],
+    ["name" => "DC Ply Mens 2016/2017 Snowboard", "categories" =>"Доски и лыжи","price" => 159999,"img_url" => "img/lot-2.jpg"],
+    ["name" => "Крепления Union Contact Pro 2015 года размер L/XL", "categories" =>"Крепления","price" => 8000,"img_url" => "img/lot-3.jpg"],
+    ["name" => "Ботинки для сноуборда DC Mutiny Charocal", "categories" =>"Ботинки","price" => 10999,"img_url" => "img/lot-4.jpg"],
+    ["name" => "Куртка для сноуборда DC Mutiny Charocal", "categories" =>"Одежда","price" => 7500,"img_url" => "img/lot-5.jpg"],
+    ["name" =>"Маска Oakley Canopy", "categories" =>"Разное","price" => 5400,"img_url" => "img/lot-6.jpg"]
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -52,7 +69,6 @@ $user_name = 'Даниил';
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
-            <!--заполните этот список из массива категорий-->
             <li class="promo__item promo__item--boards">
                 <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
             </li>
@@ -63,17 +79,17 @@ $user_name = 'Даниил';
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <!--заполните этот список из массива с товарами-->
+            <?php foreach($goods as $good):?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?= $good['img_url']?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$good["categories"]?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$good["name"]?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
+                            <span class="lot__amount"><?=$good["price"]?></span>
                             <span class="lot__cost">цена<b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
@@ -82,6 +98,7 @@ $user_name = 'Даниил';
                     </div>
                 </div>
             </li>
+            <?php endforeach;?>
         </ul>
     </section>
 </main>
@@ -90,10 +107,11 @@ $user_name = 'Даниил';
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполните этот список из массива категорий-->
+            <?php foreach($categories as $category):?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?= $category ?></a>
             </li>
+            <?php endforeach;?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
